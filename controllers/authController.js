fd6dc694b62a771b98ee161aa63d3e437e2f9069
Kapil110248 <kapilpatidarkapil0812@gmail.com>
2025-05-03 01:24:54 +0530

    Updated expirresIn

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,7 @@ exports.login = async (req, res) => {
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
+      expiresIn: "2h",
     });
     res.json({ token });
   } catch (err) {
